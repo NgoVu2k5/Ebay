b14d9dc83e533efcf9c7cbddc583dff4e0a5a3cd
NgoVu <87082175+NgoVu2k5@users.noreply.github.com>
2023-05-04 10:08:51 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,278 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+        
+
+    <link rel="stylesheet" href="assets/bootstrap.min.css">
+
+    <script src="assets/jquery.min.js"></script>
+
+    <link rel="shortcut icon" href="https://cdn4.iconfinder.com/data/icons/danger-problems-rounded/512/xxx000-512.png" type="image/x-icon">
+
+    <script src="assets/bootstrap.min.js"></script>
+
+    <link rel="stylesheet" href="assets/style.css">
+
+    <script src="assets/exec.js"></script>
+
+    <title>eBay Vaild Email Checker | By XWarnDreaM_Checkers</title>
+
+</head>
+
+<body>
+
+<div id="wrapper">
+
+    <header>
+
+        <br><br>
+
+        <center>
+
+            <div class="stats">
+
+                <font size="6" color="white" style="text-shadow: lime 0 0 15px;"><b>[+] eBay Valid Email Checker By XWarnDreaM_Checkers
+
+[+]</b></font>
+
+                <br>
+
+                <font size="4" color="gold" style="text-shadow: lime 0 0 15px;"><b>[+] Copyright: OuSsama Arrad & Ridwane Siyass
+
+[+]</b></font>
+
+                <font size="4" color="red" style="text-shadow: lime 0 0 15px;"><b>[+] Youtube : Warrning
+
+                <font size="4" color="blue" style="text-shadow: lime 0 0 15px;"><b>[+] FACEbOOK : https://www.facebook.com/DreaMOuss1
+
+[+]</b></font></div>
+
+        </center>
+
+    </header>
+
+<?php
+
+    /**
+
+EBay Email Checker
+
+ 
+
+    **/
+
+    @set_time_limit(0);
+
+    function curl_($POSTFIELDS){
+
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, "https://reg.ebay.com/reg/ajax");
+
+        curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36");
+
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $POSTFIELDS);
+
+        curl_setopt($ch, CURLOPT_REFERER, "https://reg.ebay.com/reg/PartialReg??_trksid=m570.l2621");
+
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 3);
+
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+
+        $result = curl_exec($ch);
+
+        return $result;
+
+        curl_close($ch);
+
+    }
+
+?>
+
+ 
+
+<html>
+
+    <head>
+
+    <title>Ebay Email CheCker</title>
+
+    <style>
+
+    body{background-color: #bff3ff;
+
+background: url(http://www.glennsmovers.co.uk/bgc.jpg no-repeat center center fixed;
+
+                                background-size: cover;
+
+                                    -webkit-background-size: cover;
+
+                                    -moz-background-size: cover;
+
+                                    -o-background-size: cover;
+
+    }
+
+    #text{
+
+    width:800px;
+
+    height:200px;
+
+    }
+
+    input[type=submit]{padding:2px 20px;
+
+    background:#0397d5;
+
+    color:#c6b1b1;
+
+    border:0 none;
+
+    cursor:pointer;
+
+    -webkit-border-radius: 5px;
+
+    border-radius: 5px;}
+
+    input[type=text]{
+
+    padding-bottom:4px;
+
+    padding-top:4px;
+
+    color:green;
+
+    border:0 none;
+
+    -webkit-border-radius: 1px;
+
+    border-radius: 2px;
+
+    }
+
+    input[type=submit]:hover{ background-color: #78fc81;  }
+
+    </style>
+
+    </head>
+
+    <body>
+
+   
+
+        <div style="width:950px; margin:0 auto; background-color:;">
+
+            <div align="center">
+
+                <h3> List Email To Check (1)</h3>
+
+                <form method="POST" name="praga" action="">
+
+                    <textarea id="text" name="mail"><?php if(isset($_POST['mail'])){ echo $_POST['mail']; } ?></textarea><br>
+
+                    <input type="submit" value="Check" name="sub" />
+
+                </form>
+
+            </div>
+
+        </div>
+
+ 
+
+    <?php
+
+        if(isset($_POST['sub'])){
+
+            $email_list=$_POST['mail'];
+
+            $line = explode("\r\n",$email_list);
+
+            $line = array_unique($line);
+
+            $j=0;$k=0;$o=0;
+
+            for($i=0;$i<count($line);$i++){
+
+                if (filter_var($line[$i], FILTER_VALIDATE_EMAIL)) {
+
+                    echo "<font color='green'> valide ==> </font>".$line[$i]."...";
+
+                        $rez=curl_("email=".$line[$i]."&countryId=1&mode=5&eId=email");
+
+                        if (strpos($rez,'Your email address is already registered with eBay' ) ){
+
+                        echo "<font color='green'> Ok . <br> </font>";
+
+                        $live[$j]=$line[$i];
+
+                        $j++;
+
+                        flush();
+
+                    }else{
+
+                        echo "<font color='red'> Noo . <br> </font>";
+
+                        $die[$o]=$line[$i];
+
+                        $o++;
+
+                    }
+
+                }else{
+
+                echo "<font color='red'> Invalide mail </font>=>".$line[$i]."<br>";
+
+                $not[$k]=$line[$i];
+
+                $k++;
+
+                }
+
+            flush(); ob_flush();
+
+           
+
+            }
+
+        ?>
+
+        <table border="0" width="100%">
+
+            <tr>
+
+            <td align='center' style="color:green"> Ebay emails (<?php echo @count($live);?>)</td>
+
+            <td align='center' style="color:red"> Not Ebay Eamils (<?php echo @count($die);?>)</td>
+
+            <td align='center' style="color:orange"> Invalid emails (<?php echo @count($not);?>)</td>
+
+            </tr>
+
+        <?php
+
+        if(isset($live)){ echo "<tr><td align='center' ><textarea cols='43' rows='10'>";for($i=0;$i<count($live);$i++){echo $live[$i]."\n"; } echo "</textarea></td>";}
+
+        if(isset($die)){ echo "<td align='center' ><textarea cols='43' rows='10'>";for($i=0;$i<count($die);$i++){echo $die[$i]."\n"; } echo "</textarea></td>";}else{echo "<td align='center' ><textarea cols='43' rows='10'></textarea>"; }
+
+        if(isset($not)){ echo "<td align='center' ><textarea cols='43' rows='10'>";for($i=0;$i<count($not);$i++){echo $not[$i]."\n"; } echo "</textarea></td><tr></table>";}else{echo "<td align='center' ><textarea cols='43' rows='10'></textarea>";}
+
+        }  
+
+    ?>
+
+</body>
+
+</html>
+
